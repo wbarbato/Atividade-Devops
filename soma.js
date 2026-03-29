7114d55d1f96e109a945baabbfbfb19fa34df5ec
Wellington <wellington.barbato2019@gmail.com>
2026-03-29 15:27:16 -0300

    coloquei sss

--- soma.js
+++ soma.js
@@ -1,6 +1,6 @@
 function somar(x, y) {
   if (isNaN(x) || isNaN(y)) {
-    throw new Error("Os valores precisam ser numéricos!");
+    throw new Error("Os valores precisam ser numéricosss!");
   }
 
   const resultado = x + y;
