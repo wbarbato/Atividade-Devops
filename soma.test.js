522c8e5a0a75bb37df871ceb5616e83d4936ea7b
Wellington <wellington.barbato2019@gmail.com>
2026-03-29 13:24:50 -0300

    teste push

--- soma.test.js
+++ soma.test.js
@@ -1,9 +1,9 @@
 const { somar } = require('./soma');
 
 test("Deve somar dois números corretamente", () => {
-  const resultado = somar(5, 5);
+  const resultado = somar(5, 6);
 
-  expect(resultado).toBe("A soma de 5 + 5 é igual a 10");
+  expect(resultado).toBe("A soma de 5 + 5 é igual a 11);
 });
 
 test("Deve lançar erro se não for número", () => {
