c02f9b4dc2d72cfb05cfb9fdd8c724e482bc9219
Wellington <wellington.barbato2019@gmail.com>
2026-03-29 15:16:02 -0300

    fiz agora

--- soma.test.js
+++ soma.test.js
@@ -6,8 +6,12 @@ test("Deve somar dois números corretamente", () => {
   expect(resultado).toBe("A soma de 5 + 5 é igual a 10");
 });
 
-test("Deve lançar erro se não for número", () => {
-  expect(() => {
-    somar("a", 5);
-  }).toThrow("Os valores precisam ser numéricos!");
+test("Deve lançar erro se x não for número", () => {
+  expect(() => somar("a", 5))
+    .toThrow("Os valores precisam ser numéricos!");
+});
+
+test("Deve lançar erro se y não for número", () => {
+  expect(() => somar(5, "b"))
+    .toThrow("Os valores precisam ser numéricos!");
 });
