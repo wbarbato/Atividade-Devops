6b7d791d0c036dadbc205d6fac780dd4769cfff0
Wellington <wellington.barbato2019@gmail.com>
2026-03-29 14:17:01 -0300

    Atualizando soma e testes

--- soma.test.js
+++ soma.test.js
@@ -3,7 +3,7 @@ const { somar } = require('./soma');
 test("Deve somar dois números corretamente", () => {
   const resultado = somar(5, 5);
 
-  expect(resultado).toBe("A soma de 5 + 5 é igual a 10);
+  expect(resultado).toBe("A soma de 5 + 5 é igual a 10");
 });
 
 test("Deve lançar erro se não for número", () => {
