baef6615b6c1a6e238bd5a04fb4df69f81eaf923
Wellington <wellington.barbato2019@gmail.com>
2026-03-29 11:58:17 -0300

    agora o primeiro commit

---
+++ soma.js
@@ -0,0 +1,11 @@
+function somar(x, y) {
+  if (isNaN(x) || isNaN(y)) {
+    throw new Error("Os valores precisam ser numéricos!");
+  }
+
+  const resultado = x + y;
+
+  return `A soma de ${x} + ${y} é igual a ${resultado}`;
+}
+
+module.exports = { somar };
---
+++ soma.test.js
@@ -0,0 +1,13 @@
+const { somar } = require('./soma');
+
+test("Deve somar dois números corretamente", () => {
+  const resultado = somar(5, 5);
+
+  expect(resultado).toBe("A soma de 5 + 5 é igual a 10");
+});
+
+test("Deve lançar erro se não for número", () => {
+  expect(() => {
+    somar("a", 5);
+  }).toThrow("Os valores precisam ser numéricos!");
+});
